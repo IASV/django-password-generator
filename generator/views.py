a87d111eb0b7c88ec5e5e2f9d5edfbad48fae00b
IASV <iasuarez@unicesar.edu.co>
2022-08-13 11:59:52 -0500

    numbers and special characters added

--- generator/views.py
+++ generator/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
+from random import choice
 
 # Create your views here.
 def about(request):
@@ -9,4 +10,19 @@ def home(reqeust):
     return render(reqeust, 'generator/home.html')
 
 def generate(request):
-    return render(request, 'generator/password.html')
+    
+    characters = list('abcdefghijklmnopqrstuvwxyz')
+    generated_password = ''
+    length = int(request.GET.get('lenght'))
+    
+    if request.GET.get('uppercase'):
+        characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+    if request.GET.get('special'):
+        characters.extend(list('!@#$%^&*()'))
+    if request.GET.get('numbers'):
+        characters.extend(list('0123456789'))
+    
+    for x in range(length):
+        generated_password += choice(characters) 
+    
+    return render(request, 'generator/password.html', {'password': generated_password})
